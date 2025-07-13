71f2be2427085f3f8b49cc6bf59c05b6207143e7
Suryadeep1234 <gm773328@gmail.com>
2025-07-13 22:13:56 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -570,7 +570,7 @@ async def start(client, message):
     size=get_size(files["file_size"])
     f_caption=files["caption"]
     if CUSTOM_FILE_CAPTION:
-       try:
+        try:
            f_caption=CUSTOM_FILE_CAPTION.format(file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='' if f_caption is None else f_caption)
         except:
             f_caption=f_caption
