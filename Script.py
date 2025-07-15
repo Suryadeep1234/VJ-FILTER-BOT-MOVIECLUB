eb3600711af84162e676e1c1229580f1c02d36c2
Suryadeep1234 <gm773328@gmail.com>
2025-07-15 09:04:20 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -282,7 +282,6 @@ IMDb Data:
 📆 Release Info : {release_date}
 🎛 Countries : <code>{countries}</code>
 
-
 ⏰Result Shown in: {remaining_seconds} <i>seconds</i> 🔥
 
 Requested by : {message.from_user.mention}</b>"""
